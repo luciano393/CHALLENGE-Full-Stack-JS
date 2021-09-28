c747e26379f9f46b2f9cca32a43d41c8ffe69a63
luciano393 <luciano39340268@gmail.com>
2021-09-28 00:42:05 -0300

    Create Login.js

---
+++ payments/src/pages/Auth/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Title } from '../../components/Titles/Title'
+
+export const Login = () => {
+    return (
+        <div className="Login">
+            <header>
+                <Title title="Payments"/>
+            </header>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
